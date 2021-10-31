88ff40de92e39e4814e777746406e37e1d2a06dd
Dmytro Tereshchenko <legat4life@gmail.com>
2021-10-31 18:49:39 +0200

    Add project files.

---
+++ BikeShop.Domain/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BikeShop.Domain
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [DisplayName("Category")]
+        public int? CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
